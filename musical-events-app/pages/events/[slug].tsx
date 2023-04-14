f82e741182679b6288e374d22bc9b0ea5e928e40
Mateusz Kudraj <mkudr@softserveinc.com>
2023-04-14 15:08:28 +0200

    pages and slug added

---
+++ musical-events-app/pages/events/[slug].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventPage = () => {
+  return (
+    <div>My event</div>
+  )
+}
+
+export default EventPage
---
+++ musical-events-app/pages/events/add.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddEventPage = () => {
+  return (
+    <div>add</div>
+  )
+}
+
+export default AddEventPage
